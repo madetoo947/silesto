1445e9f756ce9e64d58d9ef38f0af1871ed17c69
madetoo947 <madetoo947@gmail.com>
2025-09-04 19:08:53 +0300

    Delete wp-content/plugins directory

--- wp-content/plugins/indeed-membership-pro/admin/includes/elements/elementor-message.php
+++
@@ -1,5 +0,0 @@
-<div class="ihc-error-global-dashboard-message ihc-admin-dashboard-notice-mk-message">
-    <div class='ihc-close-notice ihc-js-close-admin-dashboard-mk-notice' data-name='elementor'>x</div>
-    <?php echo esc_html__( "Теперь вы можете ограничивать виджеты Elementor с помощью Ultimate Membership Pro, предоставляя доступ только определенным участникам или зарегистрированным пользователям. Зацените: ", 'ihc' );?>
-    <a href="<?php echo esc_url( 'https://ultimatemembershippro.com/addon-link/elementor-widget-lock/' );?>" target="_blank">Ultimate Membership Pro - Elementor Widget Lock</a>
-</div>
